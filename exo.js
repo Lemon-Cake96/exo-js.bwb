11ee27a026b3aa379f74bd4c383b4f58586e57bc
lemon-cake <kaique-lemos@hotmail.com>
2024-10-11 10:27:11 +0200

    empty variables

---
+++ exo.js
@@ -0,0 +1,15 @@
+let monCanard = "Donald";
+
+let nombreDePate;
+let nombreDAile;
+
+function canard (nombreDePate,nombreDAile){
+    let maCannette = "Daisy";
+    let mesCanards = maCannette + " et " + monCanard;
+
+
+    return nombreDePate += nombreDAile;
+}
+
+console.log(canard(2,2))
+console.log(canard("deux","deux"))
