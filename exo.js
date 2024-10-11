a2c1b34c72762b0db9f3e4c17b1179d56aafd04f
lemon-cake <kaique-lemos@hotmail.com>
2024-10-11 12:05:57 +0200

    whole plaque done

--- exo.js
+++ exo.js
@@ -29,4 +29,29 @@ f();
  f, the variable "x" exists within the function as a different variable independantly
  from the "const x"*/
 
- 
+const lettre = "AZERTYUPQSDFGHJKLMWXCVBN";
+const nombre = "0123456789";
+let plaque ="";
+
+
+for (let i=0; i < 2 ; i++){
+    plaque += lettre.charAt(Math.floor(Math.random() * lettre.length));
+    //console.log(plaque)
+}
+plaque += "-";
+
+//console.log(plaque);
+
+for (let i=0; i < 3 ; i++){
+    plaque += nombre.charAt(Math.floor(Math.random() * nombre.length));
+    //console.log(plaque)
+}
+plaque += "-";
+
+//console.log(plaque);
+
+for (let i=0; i < 2 ; i++){
+    plaque += lettre.charAt(Math.floor(Math.random() * lettre.length));
+}
+
+console.log(plaque)
